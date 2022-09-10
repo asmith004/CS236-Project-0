85029a836052df70d5658bf62d68d0f3c8b37517
Andrew <andrew@Andrews-MacBook-Pro-51.local>
2022-09-09 20:26:23 -0600

    Updating Inital Folders

---
+++ cmake-build-debug/CMakeFiles/CS236_Project_0.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/Book.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/Book.cpp.o"
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/BookCreator.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/BookCreator.cpp.o"
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/BookListCreator.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/BookListCreator.cpp.o"
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/Info.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/Info.cpp.o"
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/Library.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/Library.cpp.o"
+  "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/main.cpp" "/users/guest/s/smitha99/tmp/tmp.Ixd0Aafj7q/cmake-build-debug/CMakeFiles/CS236_Project_0.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CS236_Project_0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS236_Project_0.
+# This may be replaced when dependencies are built.
